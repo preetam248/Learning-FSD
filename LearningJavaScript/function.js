72c9d8314461952a48428b239f037a2ea4a90400
Preetam <preetamdas248@gmail.com>
2024-11-25 16:00:06 +0530

    add

--- LearningJavaScript/function.js
+++ LearningJavaScript/function.js
@@ -68,7 +68,7 @@ function addTwoNumber(a,b) {
 
 
 
-//* Impure function :  Will gave different output for same input. This function has dependency on values outside the function. There will be side effect
+//* Impure function : Will gave different output for same input. This function has dependency on values outside the function. There will be side effect
     
 let count = 0;
 
@@ -80,3 +80,51 @@ function counter(params) {
 // console.log(counter(1));
 // console.log(counter(1));
 // console.log(counter(1));
+
+
+//* Callback function: I will define a function and call it when needed. It's a behavior of function. It's note a typeof function.
+
+function printResult(result) {
+    console.log(`The result is ${result}`);
+}
+
+function calculation(operation, a, b, callback) {
+    let result = 0;
+    if (operation === "add") {
+        result = a + b;
+    } else if (operation === "sub") {
+        result = a - b;
+    } else {
+        console.log("Invalid operation");
+        return;
+    }
+
+    callback(result);
+}
+
+// calculation("add", 5, 7, printResult);
+// calculation("sub", 12, 9, printResult);
+
+//* Higher Order function: A function can be passed as argument or a function can return another function.
+
+function summation(a, b) {
+    let result = a + b;
+
+    return function () {
+        console.log(result);
+    }
+}
+
+// summation(5, 2)();
+
+//* Function Curry: Splitting a function arguments into multiple parts.
+
+function addition(a) {
+    return function (b) {
+        return function (c) {
+            return a + b + c;
+        }
+    }
+}
+
+console.log(addition(1)(2)(3));
